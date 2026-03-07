5cf08b80a0581024347365445f7a8c26cff8f292
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-03-07 21:02:41 +0530

    Basic Setup

--- Basic Setup/main.py
+++ Basic Setup/main.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.geometry("500x500")
-window.title("My First GUI")
+window = tk.Tk()        # instantiate the window
+window.geometry("500x500")          #change window dimensions
+window.title("My First GUI")        #title of window i.e., displayed on above bar
 
-icon = tk.PhotoImage(file="logo.png")
-window.iconphoto(True, icon)
+icon = tk.PhotoImage(file="logo.png")           #to change icon of window, it takes only PhotoImage format picture, so convert png into one firstlu
+window.iconphoto(True, icon)           #replace icon with new one
 
-window.configure(bg="blue")
+window.config(bg="blue")        #congiguring the window's background colour
 
 window.mainloop()
